d4b0eaaa5cadbfda2a31cba1e4e409b8db9e9445
Кристиян Николов <102967190+kristiqnnikolov@users.noreply.github.com>
2025-02-08 06:33:38 +0200

    Update page_scraper.py

--- page_scraper.py
+++ page_scraper.py
@@ -2,15 +2,12 @@ from bs4 import BeautifulSoup
 from utils import headers
 
 async def scrape_anchors(page):
-    """Scrape all anchor tags containing a div with the 'image hint' class."""
     content = await page.content()
     soup = BeautifulSoup(content, "html.parser")
 
-    # Find the "next" button for pagination
     next_button = soup.find(class_="next")
     next_link = next_button.find("a").get("href") if next_button else None
 
-    # Find all laptop detail page links
     anchor_tags = soup.select("a > .image.hint")
     hrefs = [anchor_tag.find_parent("a").get("href") for anchor_tag in anchor_tags if anchor_tag.find_parent("a")]
 
@@ -18,7 +15,6 @@ async def scrape_anchors(page):
 
 
 async def visit_pages_and_scrape(hrefs, page, laptops, scraped_items):
-    """Visit each href and scrape additional data from each laptop's page."""
     for link in hrefs:
         href = "https://ardes.bg/" + link
         await page.goto(href, {"waitUntil": "networkidle2"})
@@ -26,18 +22,15 @@ async def visit_pages_and_scrape(hrefs, page, laptops, scraped_items):
         content = await page.content()
         soup = BeautifulSoup(content, "html.parser")
 
-        # Initialize a dictionary to hold the laptop data
         laptop = {}
 
-        # Get laptop name (first part of h1 content before the comma)
         name = soup.find("h1").text.split(",")[0].strip()
         laptop["Име"] = name
 
-        # Find table body with specifications
         tbody = soup.find("tbody")
         for tr in tbody.find_all("tr"):
             header = tr.find("th").text.strip(":").strip()
-            if header not in headers:  # Skip unwanted headers
+            if header not in headers: 
                 continue
             data = tr.find_all("td")[-1].text.strip()
             laptop[header] = data
